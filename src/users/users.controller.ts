a475ad52cc126b414c4e626662d4ae4f906f80c6
Maksym Berkovskyi <maksym.berkovskyi@gmail.com>
2026-02-08 16:17:36 +0200

    feat: add get-users endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersQueryDto } from './dto/get-users.query.dto';
 
 @Controller('api/v1')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Get('get-user/:id')
   async getUserById(@Param('id') id: string) {
     return this.usersService.getUserById(id);
+  }
+
+  @Get('get-users')
+  async getUsers(@Query() query: UsersQueryDto) {
+    return this.usersService.getUsers(query);
   }
 }
