55c23e3b57c7a4ca83fdeb563752b204b837aa78
Maksym Berkovskyi <maksym.berkovskyi@gmail.com>
2026-02-08 15:20:55 +0200

    feat: ad dto for users query

---
+++ src/users/dto/get-users.query.dto.ts
@@ -0,0 +1,28 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, IsString, Min } from 'class-validator';
+
+export class UsersQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+}
